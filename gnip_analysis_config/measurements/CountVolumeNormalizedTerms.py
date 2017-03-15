9d4c7b615d32cb1a705891d8f4a8776e80dd3418
Fiona Pigott <fpigott@twitter.com>
2017-03-15 00:04:04 +0000

    changed scale factor for volume normalizations

--- gnip_analysis_config/measurements/CountVolumeNormalizedTerms.py
+++ gnip_analysis_config/measurements/CountVolumeNormalizedTerms.py
@@ -1,5 +1,5 @@
 import fileinput
-from gnip_analysis_config.measurements.measurement_base import MeasurementBase, TokenizedBody
+from gnip_analysis_config.measurements.measurement_base import MeasurementBase, TokenizedBody, HashtagsBody
 
 class CountVolumeNormalizedTerms(MeasurementBase, TokenizedBody):
     """
@@ -17,6 +17,8 @@ class CountVolumeNormalizedTerms(MeasurementBase, TokenizedBody):
         self.token_volumes_by_category = {}
         for category in self.normalizing_volumes:
             self.token_volumes_by_category[category] = {k:0 for k in self.terms}
+        if not hasattr(self, 'constant_multiplier'):
+            self.constant_multiplier = 1
     def update(self,tweet):
         """add a Tweet to the measurement"""
         # count the tokens in the Tweet
@@ -45,20 +47,17 @@ class CountVolumeNormalizedTerms(MeasurementBase, TokenizedBody):
     def get(self):
         """return time series for each category, 
            scaled by that category volume in the decahose"""
-        # I want the results to be ~O(1), so lets come up with a scaling const for all of them
-        # scaling const = normalizing_volumes[total]/token_volumes_by_category[total]
-        total_norm_vols = self.normalizing_volumes["total"]
-        total_all_tokens = sum(self.token_volumes_by_category["total"].values())
         results = []
         for category in self.token_volumes_by_category:
             category_vol = self.normalizing_volumes[category]
             for token in self.token_volumes_by_category[category]:
                 count = self.token_volumes_by_category[category][token]
-                name = token + "/" + category + "*" + str(total_norm_vols) + "/" + str(total_all_tokens)
                 if category_vol == 0:
                     results.append((0.0, name))
+                    results.append((0.0, token))
                 else:
-                    results.append(((count*total_norm_vols)/(category_vol*total_all_tokens), name))
+                    results.append(((count*self.constant_multiplier)/category_vol, token+"/"+category+"%"))
+                    results.append((count, token+" "+category+" count"))
         return results
     def get_normalization_info(self,date_key):
         raise NotImplementedError
--- gnip_analysis_config/measurements/sample_normalization_measurements.py
+++ gnip_analysis_config/measurements/sample_normalization_measurements.py
@@ -11,9 +11,16 @@ terms_list = ["people","young","love"]
 config_kwargs  = {
     # list of terms to count
     "terms": terms_list,
-    # (only relevant for CountUniqueUsersPerTerm) if true, use HLL to count cardinality of user sets, if false, count exactly
+    # (only relevant for CountVolumeNormalizedTerms-derived classes)
+    # diving the count of the word "love" by the count of all Tweets can result in an extremely small number
+    # if you like, multiply the result by a constant factor to avoid results on the order 10^-6
+    # if not, it will be "1" (no multiplier on the result)
+    "constant_multiplier": 1000000,
+    # (only relevant for CountUniqueUsersPerTerm) 
+    # if true, use HLL to count cardinality of user sets, if false, count exactly
     "hll": True,
-    # (only relevant for CountUniqueUsersPerTerm) size of the HLL obj, bigger == more memory usage, more accuracy
+    # (only relevant for CountUniqueUsersPerTerm) 
+    # size of the HLL obj, bigger == more memory usage, more accuracy
     "hll_size": 5
     }
 
