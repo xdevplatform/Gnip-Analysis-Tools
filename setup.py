bb82f4fea0bfa011b1fcacda11e38c03999e4b1a
jeffakolb <jeffakolb@gmail.com>
2017-08-28 13:39:21 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='gnip_analysis_tools',
+setup(name='Gnip-Analysis-Tools',
         packages=find_packages(),
         version='0.1',
         license='MIT',
@@ -8,5 +8,5 @@ setup(name='gnip_analysis_tools',
         author_email='jeffakolb@gmail.com',
         description='Configuration tools for Gnip-Analysis-Pipeline',  
         extras_require = {'nltk' : 'nltk'},
-        url='https://github.com/jeffakolb/Gnip-Analysis-Tools',
+        url='https://github.com/tw-ddis/Gnip-Analysis-Tools',
         )
